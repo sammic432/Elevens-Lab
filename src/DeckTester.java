32d229b3884d1cc47905be7afb6ac758e798a00f
sammic432 <sammic432@gmail.com>
2017-03-17 09:08:40 -0700

    finished activity 1&2

--- src/DeckTester.java
+++ src/DeckTester.java
@@ -11,7 +11,7 @@ public class DeckTester {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
 		String[] rank = {"2", "5", "1", "4"};
 		String[] suit = {"heart", "diamond"};
-		int[] value = {2, 5, 1, 4};
+		int[] value = {2, 5, 1};
 		
 		Deck deck1 = new Deck(rank, suit, value);
 		System.out.println(deck1.size());
